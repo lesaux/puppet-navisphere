4d7c86b77b63d441a5d608bf4c7ce78c06f6d9b8
Pierig Le Saux <lesaux@pythian.com>
2014-12-02 14:03:36 -0500

    fixing provider for redhat

--- manifests/init.pp
+++ manifests/init.pp
@@ -24,7 +24,7 @@ class navisphere {
       }->
       package { 'navisphere':
         ensure   => installed,
-        provider => dpkg,
+        provider => rpm,
         source   => '/tmp/NaviCLI-Linux-64-x86-en_US-7.30.15.0.44-1.x86_64.rpm',
         unless   => '/usr/bin/test -d /opt/Navisphere/bin/naviseccli',
       }
