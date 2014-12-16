87cbd1f450860fd1b7c4fbacc92ec30154c7c3f4
Pierig Le Saux <lesaux@pythian.com>
2014-12-16 15:15:05 -0500

    trying something else

--- manifests/init.pp
+++ manifests/init.pp
@@ -15,13 +15,13 @@ class navisphere {
       }
     }
     'RedHat': {
+      ensure_packages(['glibc.i686','nss-softokn-freebl.i686','nss-softokn-freebl.x86_64'])
       file { '/tmp/NaviCLI-Linux-64-x86-en_US-7.30.15.0.44-1.x86_64.rpm':
         ensure   => file,
         source   => 'puppet:///modules/navisphere/NaviCLI-Linux-64-x86-en_US-7.30.15.0.44-1.x86_64.rpm',
         owner    => root,
         group    => root,
       }->
-      ensure_packages(['glibc.i686','nss-softokn-freebl.i686','nss-softokn-freebl.x86_64'])
       package { 'NaviCLI-Linux-64-x86-en_US-7.30.15.0.44-1':
         ensure   => installed,
         provider => yum,
